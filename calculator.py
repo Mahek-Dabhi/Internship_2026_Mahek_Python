6a6a45ab152c75bf0b407bd81051ef0f8de79280
Mahek Dabhi <mahekdabhi1705@gmail.com>
2026-01-19 20:40:23 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+
+print(f"addition is :", a+b)
+print(f"Subtraction is :", a-b)
+print(f"Multiplication is :", a*b)
+print(f"Division is :", a/b)
+print(f"Modulus is :", a%b)
